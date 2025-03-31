05b153e100084420135d106c38922860444f8c77
Therengelan <anderson.alvarez.rengel.096@gmail.com>
2025-03-31 19:57:52 -0300

    prueba1

--- src/main/java/com/proyecto/sistema/controller/InicioController.java
+++ src/main/java/com/proyecto/sistema/controller/InicioController.java
@@ -2,7 +2,7 @@ package com.proyecto.sistema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//wasaaaaaaaaaaa;
 @Controller
 public class InicioController {
 
