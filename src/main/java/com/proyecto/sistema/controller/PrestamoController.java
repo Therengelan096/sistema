7a43317a054ab09ec22db928d25ffad47c138684
Arduz <arduzaliendro@gmail.com>
2025-04-09 15:09:30 -0400

    agrego menus interactivos y ventas de CRUD

--- src/main/java/com/proyecto/sistema/controller/PrestamoController.java
+++ src/main/java/com/proyecto/sistema/controller/PrestamoController.java
@@ -3,20 +3,40 @@ package com.proyecto.sistema.controller;
 import com.proyecto.sistema.model.Prestamo;
 import com.proyecto.sistema.repository.PrestamoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/prestamos")
 public class PrestamoController {
+
     @Autowired
     private PrestamoRepository prestamoRepository;
 
     @GetMapping
     public List<Prestamo> obtenerPrestamos() {
         return prestamoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Prestamo> crearPrestamo(@RequestBody Prestamo prestamo) {
+        Prestamo nuevoPrestamo = prestamoRepository.save(prestamo);
+        return new ResponseEntity<>(nuevoPrestamo, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Prestamo> actualizarPrestamo(@PathVariable int id, @RequestBody Prestamo prestamo) {
+        Optional<Prestamo> prestamoExistente = prestamoRepository.findById(id);
+        if (prestamoExistente.isPresent()) {
+            prestamo.setIdPrestamo(id); // Asegúrate de que el ID esté seteado
+            Prestamo prestamoActualizado = prestamoRepository.save(prestamo);
+            return new ResponseEntity<>(prestamoActualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
